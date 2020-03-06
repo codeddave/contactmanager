e1e8c48cf21af702fe7530e0e1ff3dd5abd85c59
David <drizzzydave@gmail.com>
2020-03-06 20:48:45 +0100

    Updates

---
+++ precache-manifest.ff2bd0ef3a52a1c76a376255e8392c93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247a736743a6c12a42c3c2008df19dd8",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "976deee2ccbe9bbc7120",
+    "url": "/contactmanager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f08a994d23ac9977de24",
+    "url": "/contactmanager/static/css/main.9a798c28.chunk.css"
+  },
+  {
+    "revision": "976deee2ccbe9bbc7120",
+    "url": "/contactmanager/static/js/2.a9221e74.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/contactmanager/static/js/2.a9221e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f08a994d23ac9977de24",
+    "url": "/contactmanager/static/js/main.dc9b3c2b.chunk.js"
+  },
+  {
+    "revision": "336d7cbce867dbb52ddd",
+    "url": "/contactmanager/static/js/runtime-main.24e92070.js"
+  }
+]);
